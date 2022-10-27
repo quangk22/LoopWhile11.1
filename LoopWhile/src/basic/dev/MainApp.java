73f7bdddbff55678abea2726996c6482232c4580
quangk22 <113655587+quangk22@users.noreply.github.com>
2022-10-27 08:16:37 +0700

    Add files via upload

---
+++ LoopWhile/src/basic/dev/MainApp.java
@@ -0,0 +1,17 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int tong = 0;
+		while (i <= 100) {
+			tong += i;
+			i++;
+
+		}
+		System.out.println("Tổng từ 1 đến 100 =" + tong);
+
+	}
+
+}
